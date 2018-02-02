a49c1f2119ec441d874f301bae34ff6b99e896e2
Thomas Meson <thomas@zen.ly>
2018-02-02 14:12:24 +0100

    ObservedQuery,ObservedBatch: add the HostInfo

--- session.go
+++ session.go
@@ -800,10 +800,11 @@ func (q *Query) execute(conn *Conn) *Iter {
 	return conn.executeQuery(q)
 }
 
-func (q *Query) attempt(keyspace string, end, start time.Time, iter *Iter) {
+func (q *Query) attempt(keyspace string, end, start time.Time, iter *Iter, conn *Conn) {
 	if gocqlDebug {
 		Logger.Printf("Attempting query: %d", q.attempts)
 	}
+
 	q.attempts++
 	q.totalLatency += end.Sub(start).Nanoseconds()
 	// TODO: track latencies per host and things as well instead of just total
@@ -815,6 +816,7 @@ func (q *Query) attempt(keyspace string, end, start time.Time, iter *Iter) {
 			Start:     start,
 			End:       end,
 			Rows:      iter.numRows,
+			Host:      conn.Address(),
 			Err:       iter.err,
 		})
 	}
@@ -1520,7 +1522,7 @@ func (b *Batch) WithTimestamp(timestamp int64) *Batch {
 	return b
 }
 
-func (b *Batch) attempt(keyspace string, end, start time.Time, iter *Iter) {
+func (b *Batch) attempt(keyspace string, end, start time.Time, iter *Iter, conn *Conn) {
 	b.attempts++
 	b.totalLatency += end.Sub(start).Nanoseconds()
 	// TODO: track latencies per host and things as well instead of just total
@@ -1540,7 +1542,8 @@ func (b *Batch) attempt(keyspace string, end, start time.Time, iter *Iter) {
 		Start:      start,
 		End:        end,
 		// Rows not used in batch observations // TODO - might be able to support it when using BatchCAS
-		Err: iter.err,
+		Host: conn.Address(),
+		Err:  iter.err,
 	})
 }
 
@@ -1689,6 +1692,9 @@ type ObservedQuery struct {
 	// Rows is not used in batch queries and remains at the default value
 	Rows int
 
+	// Host is the informations about the host that performed the query
+	Host string
+
 	// Err is the error in the query.
 	// It only tracks network errors or errors of bad cassandra syntax, in particular selects with no match return nil error
 	Err error
@@ -1710,6 +1716,9 @@ type ObservedBatch struct {
 
 	Start time.Time // time immediately before the batch query was called
 	End   time.Time // time immediately after the batch query returned
+
+	// Host is the informations about the host that performed the batch
+	Host string
 
 	// Err is the error in the batch query.
 	// It only tracks network errors or errors of bad cassandra syntax, in particular selects with no match return nil error
